15646b1485be269deb2af479888a1966216bd891
Alvy <floressuarezalvaro@gmail.com>
2020-11-06 22:07:48 -0800

    updated results page and pushing temp info

--- Assets/JS/results.js
+++ Assets/JS/results.js
@@ -26,41 +26,27 @@ function printResults(resultObj) {
   resultBody.classList.add('card-body');
   resultCard.append(resultBody);
 
-  var titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
-
   var bodyContentEl = document.createElement('p');
   bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
+    '<strong>Date:</strong> ' + resultObj.dt_txt + '<br/>';
+  bodyContentEl.innerHTML +=
+    '<strong>Temperature (Fahrenheit):</strong> ' + resultObj.main.temp_max + '<br/>';
+  bodyContentEl.innerHTML +=
+    '<strong>Wind Speed (MPH):</strong> ' + resultObj.wind.speed + '<br/>';
 
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
-
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
 
   var linkButtonEl = document.createElement('a');
   linkButtonEl.textContent = 'Read More';
   linkButtonEl.setAttribute('href', resultObj.url);
   linkButtonEl.classList.add('btn', 'btn-dark');
 
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+  resultBody.append(bodyContentEl, linkButtonEl);
 
   resultContentEl.append(resultCard);
 }
 
   function searchApi(city) {
-  var locQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+  var locQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
 
   fetch(locQueryUrl)
     .then(function (response) {
