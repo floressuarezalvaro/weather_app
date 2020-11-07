788151ba8ed995d904e899dc663b6f6e9b81bd3f
Alvy <floressuarezalvaro@gmail.com>
2020-11-06 18:04:04 -0800

    reset vars

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -3,14 +3,15 @@ var searchFormEl = document.querySelector('#search-form');
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
+  var cityInputVal = document.querySelector('#search-input').value;
+  var tokenzInputVal = document.querySelector('#tokenz-input').value;
 
-  if (!searchInputVal) {
+  if (!cityInputVal || !tokenzInputVal) {
     console.error('You need a search input value!');
     return;
   }
 
-  var queryString = './search-results.html?q=' + searchInputVal;
+  var queryString = 'data/2.5/forecast?q=' + cityInputVal + '&appid=' + tokenzInputVal;
 
   location.assign(queryString);
 }
